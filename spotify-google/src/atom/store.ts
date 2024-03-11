f8773a3125702e5848dffbe416e06049c51de399
Bui <thomas1296@live.com>
2024-03-11 10:30:26 -0400

    test

---
+++ spotify-google/src/atom/store.ts
@@ -0,0 +1,12 @@
+import { atom } from 'recoil';
+
+
+export const locationSearch = atom({
+    key: 'locationState',
+    default: ' ',
+})
+
+export const genreSearch  = atom({
+    key: 'genreSearch',
+    default: ' ',
+})
---
+++ spotify-google/types.ts
@@ -0,0 +1,4 @@
+export interface searchValues {
+    genre: string;
+    location: string;
+}
